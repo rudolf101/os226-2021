1b8dd7c813b085f8375773f2f5e5b401089454d2
IAmFunkyFrog <st085873@student.spbu.ru>
2021-09-26 09:44:09 +0300

    Little logical change

--- kernel.c
+++ kernel.c
@@ -16,16 +16,9 @@ static void sighnd(int sig, siginfo_t *info, void *ctx) {
 	ucontext_t *uc = (ucontext_t *) ctx;
 	greg_t *regs = uc->uc_mcontext.gregs;
 
-	syscall_do(regs[REG_RAX], regs[REG_RBX],
+    regs[REG_RAX] = syscall_do(regs[REG_RAX], regs[REG_RBX],
 			regs[REG_RCX], regs[REG_RDX],
 			regs[REG_RSI], (void *) regs[REG_RDI]);
-    unsigned long rax = 0;
-    __asm__ __volatile__(
-    ""
-    : "=a"(rax)
-    :
-    );
-    regs[REG_RAX] = rax;
     regs[REG_RIP] += 2;
 }
 
