6e119a69193374c1c3d08087b1fafe2c501711eb
rudolf101 <st087506@student.spbu.ru>
2021-10-20 15:25:50 +0300

    First impl

--- sched.c
+++ sched.c
@@ -1,3 +1,5 @@
+#define _GNU_SOURCE
+
 #include <limits.h>
 #include <string.h>
 #include <stdio.h>
@@ -9,16 +11,16 @@
 #include "pool.h"
 
 struct task {
-	void (*entry)(void *as);
-	void *as;
-	int priority;
-	int deadline;
+    void (*entry)(void *as);
+    void *as;
+    int priority;
+    int deadline;
 
-	// timeout support
-	int waketime;
+    // timeout support
+    int waketime;
 
-	// policy support
-	struct task *next;
+    // policy support
+    struct task *next;
 };
 
 static int time;
@@ -35,138 +37,157 @@ static int (*policy_cmp)(struct task *t1, struct task *t2);
 static struct task taskarray[16];
 static struct pool taskpool = POOL_INITIALIZER_ARRAY(taskarray);
 
+static sigset_t prev_sigmask;
+static int irq_enabled = 1;
+
 void irq_disable(void) {
-        // TODO: sigprocmask
+    if (irq_enabled) {
+        irq_enabled = 0;
+        sigset_t sig_set;
+        sigfillset(&sig_set);
+        sigprocmask(SIG_SETMASK, &sig_set, &prev_sigmask);
+    }
 }
 
 void irq_enable(void) {
-        // TODO: sigprocmask
+    if (!irq_enabled) {
+        sigprocmask(SIG_SETMASK, &prev_sigmask, NULL);
+        irq_enabled = 1;
+    }
 }
 
 static void policy_run(struct task *t) {
-	struct task **c = &runq;
+    struct task **c = &runq;
 
-	while (*c && policy_cmp(*c, t) <= 0) {
-		c = &(*c)->next;
-	}
-	t->next = *c;
-	*c = t;
+    while (*c && policy_cmp(*c, t) <= 0) {
+        c = &(*c)->next;
+    }
+    t->next = *c;
+    *c = t;
 }
 
 void sched_new(void (*entrypoint)(void *aspace),
-		void *aspace,
-		int priority,
-		int deadline) {
+               void *aspace,
+               int priority,
+               int deadline) {
 
-	struct task *t = pool_alloc(&taskpool);;
-	t->entry = entrypoint;
-	t->as = aspace;
-	t->priority = priority;
-	t->deadline = 0 < deadline ? deadline : INT_MAX;
-	t->next = NULL;
+    struct task *t = pool_alloc(&taskpool);;
+    t->entry = entrypoint;
+    t->as = aspace;
+    t->priority = priority;
+    t->deadline = 0 < deadline ? deadline : INT_MAX;
+    t->next = NULL;
 
-	if (!lastpending) {
-		lastpending = t;
-		pendingq = t;
-	} else {
-		lastpending->next = t;
-		lastpending = t;
-	}
+    if (!lastpending) {
+        lastpending = t;
+        pendingq = t;
+    } else {
+        lastpending->next = t;
+        lastpending = t;
+    }
 }
 
 void sched_cont(void (*entrypoint)(void *aspace),
-		void *aspace,
-		int timeout) {
+                void *aspace,
+                int timeout) {
 
-	if (current->next != current) {
-		fprintf(stderr, "Mulitiple sched_cont\n");
-		return;
-	}
+    if (current->next != current) {
+        fprintf(stderr, "Mulitiple sched_cont\n");
+        return;
+    }
 
-	irq_disable();
+    irq_disable();
 
-	if (!timeout) {
-		policy_run(current);
-		goto out;
-	}
+    if (!timeout) {
+        policy_run(current);
+        goto out;
+    }
 
-	current->waketime = time + timeout;
+    current->waketime = time + timeout;
 
-	struct task **c = &waitq;
-	while (*c && (*c)->waketime < current->waketime) {
-		c = &(*c)->next;
-	}
-	current->next = *c;
-	*c = current;
+    struct task **c = &waitq;
+    while (*c && (*c)->waketime < current->waketime) {
+        c = &(*c)->next;
+    }
+    current->next = *c;
+    *c = current;
 
-out:
-	irq_enable();
-}
-
-void sched_time_elapsed(unsigned amount) {
-	// TODO
-#if 0
-	int endtime = time + amount; 
-	while (time < endtime) {
-		pause();
-	}
-#endif
-}
-
-static int fifo_cmp(struct task *t1, struct task *t2) {
-	return -1;
-}
-
-static int prio_cmp(struct task *t1, struct task *t2) {
-	return t2->priority - t1->priority;
-}
-
-static int deadline_cmp(struct task *t1, struct task *t2) {
-	int d = t1->deadline - t2->deadline;
-	if (d) {
-		return d;
-	}
-	return prio_cmp(t1, t2);
+    out:
+    irq_enable();
 }
 
 static void tick_hnd(void) {
-	// TODO
+    time++;
+    while (waitq != NULL && waitq->waketime <= sched_gettime()) {
+        struct task *t = waitq;
+        waitq = waitq->next;
+        policy_run(t);
+    }
+}
+
+void sched_time_elapsed(unsigned amount) {
+    irq_disable();
+    int end_time = time + amount;
+    struct sigaction alarm_action;
+    sigaction(SIGALRM, NULL, &alarm_action);
+    while (time < end_time && alarm_action.sa_handler == tick_hnd) {
+        sigsuspend(&prev_sigmask);
+        sigaction(SIGALRM, NULL, &alarm_action);
+    }
+    irq_enable();
+}
+
+static int fifo_cmp(struct task *t1, struct task *t2) {
+    return -1;
+}
+
+static int prio_cmp(struct task *t1, struct task *t2) {
+    return t2->priority - t1->priority;
+}
+
+static int deadline_cmp(struct task *t1, struct task *t2) {
+    int d = t1->deadline - t2->deadline;
+    if (d) {
+        return d;
+    }
+    return prio_cmp(t1, t2);
 }
 
 long sched_gettime(void) {
-	// TODO: timer_cnt
+    return time + timer_cnt() / 1000;
+    // TODO: Add + when interrupted
 }
 
 void sched_run(enum policy policy) {
-	int (*policies[])(struct task *t1, struct task *t2) = { fifo_cmp, prio_cmp, deadline_cmp };
-	policy_cmp = policies[policy];
+    int (*policies[])(struct task *t1, struct task *t2) = { fifo_cmp, prio_cmp, deadline_cmp };
+    policy_cmp = policies[policy];
 
-	struct task *t = pendingq;
-	while (t) {
-		struct task *next = t->next;
-		policy_run(t);
-		t = next;
-	}
+    struct task *t = pendingq;
+    while (t) {
+        struct task *next = t->next;
+        policy_run(t);
+        t = next;
+    }
 
-	timer_init(1, tick_hnd);
+    timer_init(1, tick_hnd);
 
-	irq_disable();
+    irq_disable();
 
-	while (runq || waitq) {
-		current = runq;
-		if (current) {
-			runq = current->next;
-			current->next = current;
-		}
+    while (runq || waitq) {
+        current = runq;
+        if (current) {
+            runq = current->next;
+            current->next = current;
+        }
 
-		irq_enable();
-		if (current) {
-			current->entry(current->as);
-		} else {
-			pause();
-		}
-		irq_disable();
-	}
+        irq_enable();
+        if (current) {
+            current->entry(current->as);
+        } else {
+            pause();
+        }
+        irq_disable();
+    }
 
-	irq_enable();
+    irq_enable();
 }
--- timer.c
+++ timer.c
@@ -10,10 +10,29 @@
 
 #include "timer.h"
 
+static int get_usec_total(struct timeval *timeval) {
+    return 1000000 * timeval->tv_sec + timeval->tv_usec;
+}
+
 int timer_cnt(void) {
-	// TODO: getitimer
+    struct itimerval timer_val;
+    getitimer(ITIMER_REAL, &timer_val);
+    return   timer_val.it_interval.tv_usec + timer_val.it_interval.tv_sec * 1000000 - timer_val.it_value.tv_usec
+             - timer_val.it_value.tv_sec * 1000000;
 }
 
 void timer_init(int ms, void (*hnd)(void)) {
-	// TODO: setitimer
+    struct sigaction alrm_action = {
+            .sa_handler = hnd
+    };
+    sigaction(SIGALRM, &alrm_action, NULL);
+    struct timeval period = {
+            .tv_sec = ms / 1000,
+            .tv_usec = (ms % 1000) * 1000
+    };
+    struct itimerval itimerval = {
+            .it_value = period,
+            .it_interval = period
+    };
+    setitimer(ITIMER_REAL, &itimerval, NULL);
 }
