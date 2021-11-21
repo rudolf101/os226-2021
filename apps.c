3321d701bc7b733a212ac4b66f8af01bccc942f2
Anton Kozlov <akozlov@azul.com>
2021-11-21 15:59:50 +0300

    Clean unused co-op apps

--- apps.c
+++ apps.c
@@ -18,10 +18,6 @@ static int g_retcode;
 #define APPS_X(X) \
         X(echo) \
         X(retcode) \
-        X(pooltest) \
-        X(syscalltest) \
-        X(app) \
-        X(sched) \
 
 #define DECLARE(X) static int X(int, char *[]);
 APPS_X(DECLARE)
@@ -35,12 +31,6 @@ static const struct app {
         APPS_X(ELEM)
 #undef ELEM
 };
-
-static long reftime(void) {
-	struct timespec ts;
-	clock_gettime(CLOCK_MONOTONIC, &ts);
-	return ts.tv_sec * 1000 + ts.tv_nsec / 1000000;
-}
 
 static int os_printf(const char *fmt, ...) {
 	char buf[128];
@@ -65,54 +55,6 @@ static int retcode(int argc, char *argv[]) {
 	return 0;
 }
 
-struct app_ctx {
-        int cnt;
-} ctxarray[16];
-struct pool ctxpool = POOL_INITIALIZER_ARRAY(ctxarray);
-
-static void print(struct app_ctx *ctx, const char *msg) {
-        static long refstart;
-        if (!refstart) {
-                refstart = reftime();
-        }
-
-	printf("%16s id %d cnt %d time %ld reftime %ld ctx %p\n",
-			msg, 1 + ctx - ctxarray, ctx->cnt, sched_gettime(), reftime() - refstart, &ctx);
-	fflush(stdout);
-}
-
-static void app_burn(void *_ctx) {
-	struct app_ctx *ctx = _ctx;
-	while (1)  {
-		print(ctx, "burn");
-		for (volatile int i = 100000 * ctx->cnt; 0 < i; --i) {
-		}
-	}
-}
-
-static void app_preempt_sleep(void *_ctx) {
-	struct app_ctx *ctx = _ctx;
-	int cnt = ctx->cnt % 1000;
-	for (int i = 0; i < cnt; ++i) {
-		print(ctx, "sleep");
-		sched_sleep(ctx->cnt - cnt);
-	}
-}
-
-static int app(int argc, char* argv[]) {
-        int entry_id = atoi(argv[1]) - 1;
-
-        struct app_ctx *ctx = pool_alloc(&ctxpool);
-        ctx->cnt = atoi(argv[2]);
-
-        void (*entries[])(void*) = { app_burn, app_preempt_sleep };
-        sched_new(entries[entry_id], ctx, atoi(argv[3]));
-}
-
-static int sched(int argc, char* argv[]) {
-        sched_run(atoi(argv[1]));
-}
-
 static int exec(int argc, char *argv[]) {
 	const struct app *app = NULL;
 	for (int i = 0; i < ARRAY_SIZE(app_list); ++i) {
@@ -129,31 +71,6 @@ static int exec(int argc, char *argv[]) {
 
 	g_retcode = app->fn(argc, argv);
 	return g_retcode;
-}
-
-static int pooltest(int argc, char *argv[]) {
-	struct obj {
-		void *field1;
-		void *field2;
-	};
-	static struct obj objmem[4];
-	static struct pool objpool = POOL_INITIALIZER_ARRAY(objmem);
-
-	if (!strcmp(argv[1], "alloc")) {
-		struct obj *o = pool_alloc(&objpool);
-		printf("alloc %d\n", o ? (o - objmem) : -1);
-		return 0;
-	} else if (!strcmp(argv[1], "free")) {
-		int iobj = atoi(argv[2]);
-		printf("free %d\n", iobj);
-		pool_free(&objpool, objmem + iobj);
-		return 0;
-	}
-}
-
-static int syscalltest(int argc, char *argv[]) {
-	int r = os_printf("%s\n", argv[1]);
-	return r - 1;
 }
 
 int shell(int argc, char *argv[]) {
@@ -185,4 +102,3 @@ int shell(int argc, char *argv[]) {
 	}
 	return 0;
 }
-
