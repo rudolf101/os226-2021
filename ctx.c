ec68dcc67ef57168fd149bd12a7139f08e57c532
IAmFunkyFrog <st085873@student.spbu.ru>
2021-11-29 14:54:54 +0300

    Added define for clarifying

--- ctx.c
+++ ctx.c
@@ -2,10 +2,12 @@
 
 #include "ctx.h"
 
+#define ENDBR64 0xfa1e0ff3
+
 void ctx_make(struct ctx *ctx, void *entry, void *stack) {
         memset(ctx, 0, sizeof(*ctx));
 
-        if(*(unsigned*) entry == 0xfa1e0ff3 && (unsigned long) stack % 16 == 0) stack -= 8;
+        if(*(unsigned*) entry == ENDBR64 && (unsigned long) stack % 16 == 0) stack -= 8;
         ctx->rsp = (unsigned long) stack;
         ctx->rsp -= 8;
         *(unsigned long *)ctx->rsp = (unsigned long) entry;
