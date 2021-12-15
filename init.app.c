4cd9696820ae871c967af9b42b45c4493ab51b7b
rudolf101 <st087506@student.spbu.ru>
2021-12-15 06:28:41 +0300

    Update init.app.c

--- init.app.c
+++ init.app.c
@@ -14,27 +14,32 @@ int os_print(int fd, const char *str) {
 }
 
 int main(int argc, char* argv[]) {
-	os_write(1, "start\n", 6);
-
-	int pipe[2];
-	int ret = os_pipe(pipe);
-	if (ret < 0) {
-		os_print(2, "cannot create pipe\n");
-		os_exit(1);
+	int wait_time = 1e8;
+	int pid1 = os_fork();
+	int len = 6;
+	if(pid1 == 0) {
+	  int pid2 = os_fork();
+	  if(pid2 == 0) {
+		while(1) {
+		  os_write(1, "0 pid\n", len);
+		  for(int i = 0; i < wait_time; ++i);
+		}
+	  }
+	  while(1) {
+		os_write(1, "1 pid\n", len);
+		for(int i = 0; i < wait_time; ++i);
+	  }
 	}
-	int pid = os_fork();
-	if (pid) {
-		os_close(1);
-		os_dup(pipe[1]);
-		const char *arg[] = { "seq", "100", (char*)0 };
-		os_exec("seq", (char**)arg);
-	} else {
-		os_close(0);
-		os_dup(pipe[0]);
-		const char *arg[] = { "grep", "2", (char*)0 };
-		os_exec("grep", (char**)arg);
+	int pid2 = os_fork();
+	if(pid2 == 0) {
+	  while(1) {
+		os_write(1, "2 pid\n", len);
+		for(int i = 0; i < wait_time; ++i);
+	  }
 	}
-
-	os_print(2, "should not reach here\n");
-	os_exit(1);
+	while(1) {
+	  os_write(1, "3 pid\n", len);
+	  for(int i = 0; i < wait_time; ++i);
+	}
+	os_exit(0);
 }
